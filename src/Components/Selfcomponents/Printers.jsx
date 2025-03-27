5a819e002d3faf47d72f050630c4d6e829d4a064
abrar0124 <ssgdaraz10@gmail.com>
2025-03-27 12:02:08 -0400

    new work

--- src/Components/Selfcomponents/Printers.jsx
+++ src/Components/Selfcomponents/Printers.jsx
@@ -14,7 +14,6 @@ const Printers = ({ product }) => {
     country: false,
     type: false,
   });
-
   const handleCheckboxChange = (key, value) => {
     setFilters((prevFilters) => {
       const updatedFilters = prevFilters[key].includes(value)
